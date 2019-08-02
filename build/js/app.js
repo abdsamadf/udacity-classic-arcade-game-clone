61190098d9e24a14f738c5f4718a81a8a11eaed1
abdsamadf <abdulsamadf97@gmail.com>
2019-08-02 23:31:40 +0500

    show player and enemies

--- build/js/app.js
+++ build/js/app.js
@@ -6,23 +6,28 @@ function _defineProperties(target, props) { for (var i = 0; i < props.length; i+
 
 function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
 
-// Enemies our player must avoid
-var Bike =
+var xMovement = 101;
+var yMovement = 83; // Enemies our player must avoid
+
+var Enemy =
 /*#__PURE__*/
 function () {
-  function Bike() {
-    _classCallCheck(this, Bike);
+  function Enemy() {
+    _classCallCheck(this, Enemy);
 
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = 0;
+    this.y = 0;
+    this.speed = 1;
   } // Update the enemy's position, required method for game
   // Parameter: dt, a time delta between ticks
 
 
-  _createClass(Bike, [{
+  _createClass(Enemy, [{
     key: "update",
     value: function update(dt) {} // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
@@ -36,16 +41,55 @@ function () {
     }
   }]);
 
-  return Bike;
+  return Enemy;
 }(); // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-// Now instantiate your objects.
+
+
+var Player =
+/*#__PURE__*/
+function () {
+  function Player() {
+    _classCallCheck(this, Player);
+
+    this.sprite = "images/char-boy.png";
+    this.x = 2 * xMovement;
+    this.y = 5 * yMovement;
+  } // Update the player position
+  // Parameter: dt, a time delta between ticks
+
+
+  _createClass(Player, [{
+    key: "update",
+    value: function update() {// You should multiply any movement by the dt parameter
+      // which will ensure the game runs at the same speed for
+      // all computers.
+    }
+  }, {
+    key: "render",
+    value: function render() {
+      ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+  }, {
+    key: "handleInput",
+    value: function handleInput(allowedKeys) {}
+  }]);
+
+  return Player;
+}(); // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
 
+
+var enemy1 = new Enemy();
+var enemy2 = new Enemy();
+var enemy3 = new Enemy();
+var enemy4 = new Enemy();
+var enemy5 = new Enemy();
+var allEnemies = [enemy1, enemy2, enemy3, enemy4, enemy5];
+var player = new Player(); // This listens for key presses and sends the keys to your
+// Player.handleInput() method. You don't need to modify this.
 
 document.addEventListener('keyup', function (e) {
   var allowedKeys = {
--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
+const xMovement = 101;
+const yMovement = 83;
+
 // Enemies our player must avoid
-class Bike {
+class Enemy {
     constructor() {
         // Variables applied to each of our instances go here,
         // we've provided one for you to get started
@@ -8,6 +11,9 @@ class Bike {
         // a helper we've provided to easily load images
 
         this.sprite = 'images/enemy-bug.png';
+        this.x = 0;
+        this.y = 0;
+        this.speed = 1;
     }
 
     // Update the enemy's position, required method for game
@@ -16,6 +22,7 @@ class Bike {
         // You should multiply any movement by the dt parameter
         // which will ensure the game runs at the same speed for
         // all computers.
+
     }
 
     // Draw the enemy on the screen, required method for game
@@ -27,11 +34,41 @@ class Bike {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+class Player {
+    constructor () {
+        this.sprite = "images/char-boy.png";
+        this.x = 2 * xMovement;
+        this.y = 5 * yMovement;
+    }
 
+    // Update the player position
+    // Parameter: dt, a time delta between ticks
+    update() {
+        // You should multiply any movement by the dt parameter
+        // which will ensure the game runs at the same speed for
+        // all computers.
+    }
+
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+
+    handleInput(allowedKeys) {
+        
+    }
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+let enemy1 = new Enemy();
+let enemy2 = new Enemy();
+let enemy3 = new Enemy();
+let enemy4 = new Enemy();
+let enemy5 = new Enemy();
+let allEnemies = [enemy1, enemy2, enemy3, enemy4, enemy5 ];
+
+let player = new Player();
 
 
 // This listens for key presses and sends the keys to your
